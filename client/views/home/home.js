55403e9c3c5448b3c4da4b559dcf0101daa38ed2
richsilv@yahoo.co.uk <richardlsilverton32@googlemail.com>
2015-02-10 09:30:38 +0000

    automated some scroll interaction

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -12,26 +12,35 @@ Template.Home.rendered = function() {
 
     tl.add( TweenLite.to('.top-spacer', 2, {height: topSectionFactor * vmax}) );
     tl.add( TweenLite.to('.lois-blurb', 1, {opacity: 1}) );
-    tl.add( TweenLite.to('.lois-blurb', 1, {opacity: 0}), "+=3" );
-    tl.add( TweenLite.to('.top-spacer', 2, {height: 5.6975 * vmax}), "-=0.5" );
-    tl.add( TweenLite.to('.top-menu .centered-button', 2, {height: 10.5 * vmax, width: 10.5 * vmax}), "-=2" );
-    tl.add( TweenLite.to('.top-menu .centered-button .button-text', 2, {'font-size': 2.3 * vmax}), "-=2" );
 
-    new ScrollScene({duration: 300})
+    new ScrollScene({offset: 0, duration: 400})
+        .setTween(TweenLite.to('.lois-blurb', 1, {opacity: 0}) )
+        .addTo(controller);
+    new ScrollScene({offset: 200, duration: 800})
+        .setTween(TweenLite.to('.top-spacer', 2, {height: 5.6975 * vmax}) )
+        .addTo(controller);
+    new ScrollScene({offset: 200, duration: 800})
+        .setTween(TweenLite.to('.top-menu .centered-button', 2, {height: 10.5 * vmax, width: 10.5 * vmax}) )
+        .addTo(controller);
+    new ScrollScene({offset: 200, duration: 800})
+        .setTween(TweenLite.to('.top-menu .centered-button .button-text', 2, {'font-size': 2.3 * vmax}) )
+        .addTo(controller);
+
+    new ScrollScene({offset: 1000, duration: 600})
     	.setTween(TweenLite.to('.top-menu', 1, {height: 5.3 * vmax}))
     	.addTo(controller);
-    new ScrollScene({ duration: 300})
+    new ScrollScene({offset: 1000, duration: 600})
     	.setTween(TweenLite.to('.top-menu .centered-button', 1, {top: 2.65 * vmax}))
     	.addTo(controller);
 
-    new ScrollScene({offset: 300, duration: 300})
+    new ScrollScene({offset: 1600, duration: 600})
     	.setTween(TweenLite.to('.top-menu .menu-items .item', 1, {x: 0}))
     	.addTo(controller);
-    new ScrollScene({offset: 400, duration: 200})
+    new ScrollScene({offset: 1800, duration: 400})
     	.setTween(TweenLite.to('.top-menu .menu-items', 1, {opacity: 1}))
     	.addTo(controller);
 
-    new ScrollScene({duration: 1000})
+    new ScrollScene({duration: 2800})
 		.setPin('.splash-scene')
 		.addTo(controller);
 
